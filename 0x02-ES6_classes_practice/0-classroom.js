80a073316b1b86bcdfefe8cf7ba48365da07c545
AGBAGHER JAPHET <ternguy1995@gmail.com>
2023-04-21 03:20:25 -0700

    done

---
+++ 0x02-ES6_classes_practice/0-classroom.js
@@ -0,0 +1,6 @@
+"""this is my home practice to give perfection of the tasks"""
+export default class classRoom {
+	constructor(maxStudentsSize){
+	this.maxStudentsSize = _maxStudentsSize;
+	}
+}
---
+++ 0x02-ES6_classes_practice/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import classRoom from './0-classroom.js'
+
+export default class from  initializeRoom() {
+	const class1 = new classRoom(19);
+	const class2 = new classRoom(20);
+	const class3 = new classRoom(34);
+	const newList = [class1, class2, class3);
+return (newList);
+}
